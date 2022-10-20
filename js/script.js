83c1fc72495f56fab5616a5d7e56bf168e1d8d86
Allen-Li-hub <113055658+Allen-Li-hub@users.noreply.github.com>
2022-10-20 23:48:14 +0000

    change

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
-/* Created by:Allen
-* This file contains the Js for index.html
-*/
-function perimeter() {
-    var sideA = document.getElementById("sideA").value
-    var base = document.getElementById("base").value
-    var sideB = document.getElementById("sideB").value
-    var perimeter = parseFloat(sideA) + parseFloat(base) + parseFloat(sideB)
-    var result = document.getElementById("result")
-    .innerHTML = "Perimeter is " + perimeter
+// Created by: Allen
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+/**
+ * This function calculates area of triangle.
+ */
+function calculate() {
+  // input
+  const sidea = parseInt(document.getElementById("sidea-of-triangle").value)
+  const sideb = parseInt(document.getElementById("sideb-of-triangle").value)
+  const sidec = parseInt(document.getElementById("sidec-of-triangle").value)
+
+  // process
+  const perimeter = (sidea + sideb + sidec)
+
+  // output
+  document.getElementById("perimeter").innerHTML = "Perimeter is: " + perimeter + "cm"
 }
